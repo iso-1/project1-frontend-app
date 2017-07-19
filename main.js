aacb92b95197112ef55886557d6dd6c35a7b2302
LAPTOP-1JJJVKPB\Ryuichi <kd1242983@st.kobedenshi.ac.jp>
2017-07-19 11:31:57 +0900

    授業内容

---
+++ main.js
@@ -0,0 +1,15 @@
+"use strict;"
+
+function answer(){
+    var input1 = document.getElementById('input1');
+    var input2 = document.getElementById('input2');
+    var output = document.getElementById('output');
+
+
+
+
+
+    var result = parseInt(input1.value) + parseInt(input2.value);
+
+    output.textContent = result
+}
